6b610b42f30aae17d592cde2cdea184815395487
Evan Dunbar <evdunbar@protonmail.com>
2024-12-04 17:00:45 -0600

    Start using a matrix for genotype selection

--- analyze.R
+++ analyze.R
@@ -44,7 +44,7 @@ LEFT_OR_RIGHT <- "left"
 #   - Count down columns or across rows first?
 COLUMN_OR_ROW_FIRST <- "column"
 
-# Which fish were used?
+# Which fish were used in this assay?
 #   - Use the same order as the assay machine
 #   - You can include ranges (e.g. "A1-B3") and individual wells (e.g. "F5")
 FISH_USED <- c("A1-F2")
@@ -58,31 +58,29 @@ library(tidyverse)
 
 # turn full genotype file into a table that can be merged with the data
 process_genotypes <- function() {
+  # create matrix to help with computations
+  helper_matrix <- matrix(0:95, nrow = 8, ncol = 12, byrow = TRUE)
+
   # get correct data and convert to 0-95 labelling
   genotype_data <- read_csv(GENOTYPING_FILE) %>%
-    select(well = Well, genotype = Cluster) %>%
+    select(genotyping_well = Well, genotype = Cluster) %>%
     filter(genotype %in% c("HET", "HOM", "WT")) %>%
-    mutate(well = (match(substr(well, 1, 1), LETTERS[1:8]) - 1) * 12 + as.integer(substr(well, 2, 3)) - 1)
+    mutate(well_number = as.integer((match(substr(genotyping_well, 1, 1), LETTERS[1:8]) - 1) * 12 + as.integer(substr(genotyping_well, 2, 3)) - 1))
 
-  if (LABELLING_TYPE == 96) {
-    # don't have to change anything
-  } else if (LABELLING_TYPE == 48) {
-    # filter out the other half of the data
+  # correct to matrix to right format
+  if (LABELLING_TYPE == 48) {
     if (LEFT_OR_RIGHT == "left") {
-      genotype_data <- genotype_data %>%
-        filter(well %% 12 %in% 0:5)
+      helper_matrix <- t(helper_matrix[, 1:6])
     } else if (LEFT_OR_RIGHT == "right") {
-      genotype_data <- genotype_data %>%
-        filter(well %% 12 %in% 6:11) %>%
-        mutate(well = well - 6)
+      helper_matrix <- t(helper_matrix[, 7:12])
     } else {
       stop("LEFT_OR_RIGHT must be either \"left\" or \"right\"")
     }
 
-    # convert to 0-47 labelling
+    # filter data to the side we just specified
     genotype_data <- genotype_data %>%
-      mutate(new_well = (well %% 6) * 8 + ceiling((well + 1) / 12) - 1)
-  } else {
+      filter(well_number %in% helper_matrix)
+  } else if (LABELLING_TYPE != 96) {
     stop("LABELLING_TYPE must be either 48 or 96")
   }
 
@@ -93,45 +91,45 @@ process_genotypes()
 
 # read the file and get rid of the auto generated zantiks lines
 # they would mess up csv parsing
-# lines <- readLines(data_file)
-# csv_text <- lines[4:(length(lines) - 1)]
-# data <- read_csv(I(csv_text), col_types = "dccici")
-#
-# y_maze_analysis <- function(df) {
-#   # set up function to map zone sequences to turn directions
-#   zone_sequence_mapping <- function(zone, next_zone) {
-#     result <- case_when(
-#       zone == 1 & next_zone == 2 ~ "L",
-#       zone == 1 & next_zone == 3 ~ "R",
-#       zone == 2 & next_zone == 1 ~ "R",
-#       zone == 2 & next_zone == 3 ~ "L",
-#       zone == 3 & next_zone == 1 ~ "L",
-#       zone == 3 & next_zone == 2 ~ "R"
-#     )
-#
-#     return(result)
-#   }
-#
-#   # remove center zone and exit data, not needed for analysis
-#   cleaned_data <- data %>%
-#     filter(ZONE != 4 & ACTION != "Exit_Zone")
-#
-#   # make tetragrams and process data
-#   processed_data <- cleaned_data %>%
-#     group_by(ARENA) %>% # calculate everything per arena
-#     mutate(next_zone = lead(ZONE)) %>%
-#     filter(ZONE != next_zone) %>% # only care about zone changes
-#     mutate(turn_direction = zone_sequence_mapping(ZONE, next_zone)) %>%
-#     mutate(next_turn = lead(turn_direction)) %>%
-#     mutate(next_next_turn = lead(next_turn)) %>%
-#     mutate(next_next_next_turn = lead(next_next_turn)) %>%
-#     filter(!is.na(next_next_next_turn)) %>%
-#     mutate(tetragram = paste0(turn_direction, next_turn, next_next_turn, next_next_next_turn)) %>% # probably a better way to do this...
-#     count(tetragram, name = "count") %>%
-#     mutate(percentage = count / sum(count) * 100) %>%
-#     ungroup()
-#
-#   return(processed_data)
-# }
-#
-# y_maze_analysis(data)
+lines <- readLines(DATA_FILE)
+csv_text <- lines[4:(length(lines) - 1)]
+data <- read_csv(I(csv_text), col_types = "dccici")
+
+y_maze_analysis <- function(df) {
+  # set up function to map zone sequences to turn directions
+  zone_sequence_mapping <- function(zone, next_zone) {
+    result <- case_when(
+      zone == 1 & next_zone == 2 ~ "L",
+      zone == 1 & next_zone == 3 ~ "R",
+      zone == 2 & next_zone == 1 ~ "R",
+      zone == 2 & next_zone == 3 ~ "L",
+      zone == 3 & next_zone == 1 ~ "L",
+      zone == 3 & next_zone == 2 ~ "R"
+    )
+
+    return(result)
+  }
+
+  # remove center zone and exit data, not needed for analysis
+  cleaned_data <- data %>%
+    filter(ZONE != 4 & ACTION != "Exit_Zone")
+
+  # make tetragrams and process data
+  processed_data <- cleaned_data %>%
+    group_by(ARENA) %>% # calculate everything per arena
+    mutate(next_zone = lead(ZONE)) %>%
+    filter(ZONE != next_zone) %>% # only care about zone changes
+    mutate(turn_direction = zone_sequence_mapping(ZONE, next_zone)) %>%
+    mutate(next_turn = lead(turn_direction)) %>%
+    mutate(next_next_turn = lead(next_turn)) %>%
+    mutate(next_next_next_turn = lead(next_next_turn)) %>%
+    filter(!is.na(next_next_next_turn)) %>%
+    mutate(tetragram = paste0(turn_direction, next_turn, next_next_turn, next_next_next_turn)) %>% # probably a better way to do this...
+    count(tetragram, name = "count") %>%
+    mutate(percentage = count / sum(count) * 100) %>%
+    ungroup()
+
+  return(processed_data)
+}
+
+y_maze_analysis(data)
