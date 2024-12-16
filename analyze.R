621c7f1f86d857ef1ce62dd3ced2d2c5012ea056
E Dunbar <edunbar02@gmail.com>
2024-12-16 11:15:40 -0600

    Continue updating for multi-file

--- analyze.R
+++ analyze.R
@@ -60,7 +60,7 @@ OUTPUT_FILE <- ""
 #   - y-maze 4
 #
 # Each row corresponds to a data file
-ASSAY_NAMEs <- c(
+ASSAY_NAMES <- c(
   "y-maze 15",
   "y-maze 15",
   "y-maze 15",
@@ -73,31 +73,33 @@ ASSAY_NAMEs <- c(
   "y-maze 15"
 )
 
-# How do you want to specify fish? (sorry this is a lot of questions)
-#   - Do you want to answer in terms of 48 or 96-well plate labelling?
-#     48-well:      96-well:
-#      1 2 3 4 5 6 7 8   1  2  3  4  5  6  7  8  9 10 11 12
-#    A . . . . . . . .   A .  .  .  .  .  .  .  .  .  .  .  .
-#    B . . . . . . . .   B .  .  .  .  .  .  .  .  .  .  .  .
-#    C . . . . . . . .   C .  .  .  .  .  .  .  .  .  .  .  .
-#    D . . . . . . . .   D .  .  .  .  .  .  .  .  .  .  .  .
-#    E . . . . . . . .   E .  .  .  .  .  .  .  .  .  .  .  .
-#    F . . . . . . . .   F .  .  .  .  .  .  .  .  .  .  .  .
-#                        G .  .  .  .  .  .  .  .  .  .  .  .
-#                        H .  .  .  .  .  .  .  .  .  .  .  .
-LABELLING_TYPE <- 48
-#   - If 48-well, YOU MUST specify if left half, right half, or top left within the 96-well genotyping plate
-LOCATION_48 <- "left half"
-#   - Count down columns or across rows first?
-COLUMN_OR_ROW_FIRST <- "column"
-
-# Which fish were used in this assay?
-#   - Use the same order as the assay machine
-#   - You can include ranges (e.g. "A1-B3") and individual wells (e.g. "F5")
-# FISH_USED <- c("A1-F2")
-# FISH_USED <- c("A3-F4")
-FISH_USED <- c("A5-F6")
-# FISH_USED <- c("A7-F8")
+# Input the fish used paths
+# COUNTING_DIRECTIONS can be either across or down
+FISH_USED_PREFIX <- "2024/"
+FISH_USED <- c(
+  "11/01/ymaze_15/a/fish_used.txt",
+  "11/01/ymaze_15/b/fish_used.txt",
+  "11/01/ymaze_15/c/fish_used.txt",
+  "11/01/ymaze_15/d/fish_used.txt",
+  "11/04/ymaze_15/a/fish_used.txt",
+  "11/04/ymaze_15/b/fish_used.txt",
+  "11/04/ymaze_15/c/fish_used.txt",
+  "12/09/ymaze_15/a/fish_used.txt",
+  "12/09/ymaze_15/b/fish_used.txt",
+  "12/09/ymaze_15/c/fish_used.txt"
+)
+COUNTING_DIRECTIONS <- c(
+  "across",
+  "across",
+  "across",
+  "across",
+  "across",
+  "across",
+  "across",
+  "across",
+  "across",
+  "across"
+)
 
 
 ############
@@ -109,131 +111,35 @@ library(dplyr)
 library(readr)
 library(readxl)
 
-# turn FISH_USED into a machine readable format
-parse_fish_used <- function() {
-  is_range <- str_detect(FISH_USED, "-")
-  ranges <- str_split(FISH_USED[is_range], "-")
-  individuals <- FISH_USED[!is_range]
-
-  return(c(ranges, individuals))
-}
-
 # turn full genotype file into a table that can be merged with the data
-process_genotypes <- function() {
-  # get correct data and convert to 0-95 labelling
-  genotype_data <- read_csv(GENOTYPING_FILE) %>%
-    select(genotyping_well = Well, genotype = Cluster) %>%
-    filter(genotype %in% c("HET", "HOM", "WT")) %>%
-    mutate(well_id = as.integer((match(substr(genotyping_well, 1, 1), LETTERS[1:8]) - 1) * 12 +
-      as.integer(substr(genotyping_well, 2, 3)) - 1))
+process_genotypes <- function(genotyping_file, fish_used_file, counting_direction) {
+  # read in data about which fish were used
+  fish_used_data <- read.table(fish_used_file) %>%
+    as.matrix()
 
-  # create matrices to help with computations
-  equivalence_matrix <- matrix(0:95, nrow = 8, ncol = 12, byrow = TRUE)
-  if (LABELLING_TYPE == 96) {
-    num_rows <- 8
-    num_cols <- 12
-  } else if (LABELLING_TYPE == 48) {
-    num_rows <- 6
-    num_cols <- 8
-    if (LOCATION_48 == "left half") {
-      equivalence_matrix <- t(equivalence_matrix[, 1:6])
-    } else if (LOCATION_48 == "right half") {
-      equivalence_matrix <- t(equivalence_matrix[, 7:12])
-    } else if (LOCATION_48 == "top left") {
-      equivalence_matrix <- equivalence_matrix[1:6, 1:8]
-    } else {
-      stop("LOCATION_48 must be \"left half\", \"right half\", or \"top left\"")
-    }
-
-    # filter data to the side we just specified
-    genotype_data <- genotype_data %>%
-      filter(well_id %in% equivalence_matrix)
-  } else {
-    stop("LABELLING_TYPE must be either 48 or 96")
-  }
-  labeling_matrix <- matrix(outer(LETTERS[1:num_rows], sprintf("%d", 1:num_cols), FUN = paste0),
-    nrow = num_rows,
-    ncol = num_cols
+  # create matrix to match well labels in genotype data
+  label_matrix <- matrix(
+    outer(LETTERS[1:8],
+      sprintf("%02d", 1:12),
+      FUN = paste0
+    ),
+    nrow = 8,
+    ncol = 12
   )
 
-  # get matrix positions to access well id
-  used_matrix <- matrix(FALSE, nrow = nrow(labeling_matrix), ncol = ncol(labeling_matrix))
-  parsed_fish <- parse_fish_used()
-  ranges <- parsed_fish[[1]]
-  # iterate over each range by pair
-  for (i in 1:(length(ranges) / 2)) {
-    begin_well <- ranges[i * 2 - 1]
-    end_well <- ranges[i * 2]
-    begin_location <- which(labeling_matrix == begin_well, arr.ind = TRUE)
-    end_location <- which(labeling_matrix == end_well, arr.ind = TRUE)
-    begin_row <- begin_location[1, 1]
-    begin_col <- begin_location[1, 2]
-    end_row <- end_location[1, 1]
-    end_col <- end_location[1, 2]
-
-    if (COLUMN_OR_ROW_FIRST == "row") {
-      # are we filling whole rows?
-      # note that LABELLING_TYPE has already been validated
-      rows_full <- begin_col == 1 & end_col == num_cols
-      if (rows_full) {
-        used_matrix[begin_row:end_row, ] <- TRUE
-      } else {
-        # are we one row?
-        if (begin_row == end_row) {
-          used_matrix[begin_row, begin_col:end_col]
-        } else {
-          # first row
-          used_matrix[begin_row, begin_col:num_cols] <- TRUE
-          # last row
-          used_matrix[end_row, 1:end_col] <- TRUE
-          # middle rows
-          if (end_row - begin_row > 1) {
-            used_matrix[(begin_row + 1):(end_row - 1), ] <- TRUE
-          }
-        }
-      }
-    } else if (COLUMN_OR_ROW_FIRST == "column") {
-      # are we filling whole columns?
-      # note that LABELLING_TYPE has already been validated
-      cols_full <- begin_row == 1 & end_row == num_rows
-      if (cols_full) {
-        used_matrix[, begin_col:end_col] <- TRUE
-      } else {
-        # are we one column?
-        if (begin_col == end_col) {
-          used_matrix[begin_row:end_row, begin_col]
-        } else {
-          # first column
-          used_matrix[begin_row:num_rows, begin_col] <- TRUE
-          # last column
-          used_matrix[1:end_row, end_col] <- TRUE
-          # middle columns
-          if (end_col - begin_col > 1) {
-            used_matrix[, (begin_col + 1):(end_col - 1)] <- TRUE
-          }
-        }
-      }
-    } else {
-      stop("COLUMN_OR_ROW_FIRST must be either \"row\" or \"column\"")
-    }
-  }
-  if (length(parsed_fish) > 1) {
-    singletons <- parsed_fish[[2]]
-    for (well in singletons) {
-      used_matrix[which(labeling_matrix == well)] <- TRUE
-    }
+  # get which wells were used
+  wells_used <- label_matrix[fish_used_data == "x"]
+  if (counting_direction == "across") {
+    wells_used <- sort(wells_used)
   }
 
-  # filter to only the used fish!!!!
-  genotype_data <- genotype_data %>%
-    filter(well_id %in% equivalence_matrix[used_matrix]) %>%
-    arrange(genotyping_well)
+  # read in data that matches valid genotypes
+  genotype_data <- read_csv(genotyping_file) %>%
+    select(genotyping_well = Well, genotype = Cluster) %>%
+    filter(genotype %in% c("HET", "HOM", "WT")) %>%
+    filter(genotyping_well %in% wells_used)
 
-  # add row IDs
-  genotype_data <- genotype_data %>%
-    mutate(row_id = row_number())
-
-  return(genotype_data)
+  print(genotype_data)
 }
 
 microtracker_analysis <- function() {
@@ -317,8 +223,16 @@ y_maze_analysis <- function() {
   return(processed_data)
 }
 
-if (ASSAY_NAME == "y-maze 15") {
-  output <- y_maze_analysis()
-}
+# main loop!
+for (idx in seq_along(DATA_FILES)) {
+  # get correct files by corresponding index
+  data_file <- paste0(DATA_FILE_PREFIX, DATA_FILES[idx])
+  genotyping_file <- paste0(GENOTYPING_FILE_PREFIX, GENOTYPING_FILES[idx])
+  fish_used_file <- paste0(FISH_USED_PREFIX, FISH_USED[idx])
+  assay_name <- ASSAY_NAMES[idx]
+  counting_direction <- COUNTING_DIRECTIONS[idx]
 
-write_csv(output, "output.csv")
+  process_genotypes(genotyping_file, fish_used_file, counting_direction)
+
+  # write_csv(output, "output.csv")
+}
