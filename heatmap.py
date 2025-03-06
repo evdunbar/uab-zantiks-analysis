20a0bb34f4552ef250d56d652674f2e2f2e14bd5
E Dunbar <edunbar02@gmail.com>
2025-03-06 16:31:37 -0600

    Update heatmap to use new data utilities

--- heatmap.py
+++ heatmap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env uv run
 import math
-import re
+from typing import Iterable
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -10,14 +10,21 @@ from numpy.typing import NDArray
 from PIL import Image
 from scipy import ndimage
 
+import data_utils
+
 mpl.rc("text", usetex=True)
 
 
 class Heatmap:
     def __init__(
-        self, data_path: str, arena_map_path: str, scale_factors: tuple[NDArray]
+        self,
+        data: data_utils.ZantiksData,
+        genotypes: str | Iterable[str],
+        arena_map_path: str,
+        scale_factors: tuple[NDArray],
     ):
-        self.xy_data = self._load_xy(data_path)
+        self.info = data.info
+        self.xy_data = data.get_genotype(genotypes)
         arena_map = self._load_image(arena_map_path)
         self.arena_mask = arena_map.sum(-1) < 10
         self.x_bias = scale_factors[0][0]
@@ -25,24 +32,7 @@ class Heatmap:
         self.y_bias = scale_factors[1][0]
         self.y_scale = scale_factors[1][1]
 
-        self.num_arenas = int((self.xy_data.width - 1) / 2)
         self.map = np.zeros_like(self.arena_mask, dtype=float)
-
-        self.date = self._value_from_path(data_path, r".*(\d{4})(\d{2})(\d{2})T")
-        self.group = self._value_from_path(data_path, r".*/([abcd]+)/.*")
-
-    def _load_xy(self, filepath: str) -> pl.DataFrame:
-        """
-        - Returns columns of the form [XY]_A[0-9]+
-        - Represents X and Y tracking for Arenas
-        - Data in millimeters
-        - Top left of plate is (0, 0)
-        - Bottom right is (127.76, 85.4)
-        """
-        with open(filepath, "r") as f:
-            xy_data = pl.read_csv(f)
-
-        return xy_data
 
     def _load_image(self, filepath: str) -> NDArray:
         with open(filepath, "rb") as f:
@@ -50,17 +40,13 @@ class Heatmap:
 
         return image
 
-    def _value_from_path(self, path: str, query: str) -> tuple | None:
-        regexp = re.compile(query)
-        if (maybe_matches := regexp.search(path)) is not None:
-            return maybe_matches.groups()
-        else:
-            return None
-
     def make_map(self, *, show: bool = False):
-        for i in range(1, self.num_arenas + 1):
+        for arena_id in self.xy_data["ARENA"].unique().sort():
             arena = np.nan_to_num(
-                self.xy_data.select(f"X_A{i}", f"Y_A{i}").to_numpy().astype(float)
+                self.xy_data.filter(pl.col("ARENA") == arena_id)
+                .select("X", "Y")
+                .to_numpy()
+                .astype(float)
             )
             self._process_arena(arena)
 
@@ -87,12 +73,12 @@ class Heatmap:
 
         plot_title = r"\textbf{Social Preference}"
         save_title = "social_preference"
-        if self.group is not None:
-            plot_title += f" \\textit{{Group(s) {self.group[0].upper()}}}"
-            save_title += f"_{self.group[0].upper()}"
-        if self.date is not None:
-            plot_title += f" {self.date[1]}-{self.date[2]}-{self.date[0]}"
-            save_title += f"_{self.date[1]}-{self.date[2]}-{self.date[0]}"
+        if self.info.groups is not None:
+            plot_title += f" \\textit{{Group(s) {self.info.groups}}}"
+            save_title += f"_{self.info.groups}"
+        if self.info.day is not None:
+            plot_title += f" {self.info.month}-{self.info.day}-{self.info.year}"
+            save_title += f"_{self.info.month}-{self.info.day}-{self.info.year}"
 
         plt.figure(dpi=100)
         plt.title(plot_title)
@@ -115,16 +101,22 @@ class Heatmap:
 
 
 if __name__ == "__main__":
-    import glob
-
     import find_mapping
 
-    filenames = glob.glob("data/*/*/*/social_preference/*/*xy_position.csv")
-    mapping_finder = find_mapping.MapFinder(filenames)
+    dataloader = data_utils.DataLoader().add_by_filter(
+        assay_types=("social_preference",), data_type="position"
+    )
+    all_zantiks_data = dataloader.load_all()
+    mapping_finder = find_mapping.MapFinder(
+        [zantiks_data.info.path for zantiks_data in all_zantiks_data]
+    )
     mapping_finder.find_mappings()
     scale_factors = mapping_finder.mappings
 
-    for filename in filenames:
-        Heatmap(filename, "data/social_preference_arenas.bmp", scale_factors).make_map(
-            show=True
-        )
+    for zantiks_data in all_zantiks_data:
+        Heatmap(
+            zantiks_data,
+            ("HOM",),
+            "data/social_preference_arenas.bmp",
+            scale_factors,
+        ).make_map(show=True)
